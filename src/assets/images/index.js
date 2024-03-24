f0136cb411c6cebcf53885250a545e060810f17f
Ishika patni <132484010+ishika3838@users.noreply.github.com>
2024-03-24 01:15:20 -0700

    logo

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
-import logo from '../images/icons/logo.svg'
+//import logo from '../images/icons/logo.svg'
 import image from '../images/icons/Bookmarks-cuate.svg'
+import logo from '../images/icons/sitelogo.svg'
 export {
    
    image, 
