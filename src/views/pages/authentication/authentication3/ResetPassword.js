3ed9755fcf6bb26fb80eefcb05757ec9b9f4b9ab
Vicky2114 <vr6994056@gmail.com>
2024-03-24 08:46:39 +0530

    bugs and auth flow completed

--- src/views/pages/authentication/authentication3/ResetPassword.js
+++ src/views/pages/authentication/authentication3/ResetPassword.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Button, Container, Grid, Paper, TextField, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import { styled } from '@mui/system';
 import reset from '../../../../assets/images/auth/reset.svg';
 import AnimateButton from 'ui-component/extended/AnimateButton';
+import axios from 'axios';
+import { RESET_PASSWORD } from 'api/auth';
+import { toast } from 'react-toastify';
 
 const ResetContainer = styled(Container)(() => ({
   display: 'flex',
@@ -24,19 +27,36 @@ const ResetPassword = () => {
   // State for form input values
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-
+  const params = useParams();
+  const navigate = useNavigate();
+  const { id, token } = params;
   // State for form submission status
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   // Function to handle form submission
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      if (password !== confirmPassword) {
+        toast.error('password not match');
+      }
+      const res = await axios.post(RESET_PASSWORD(id, token), {
+        password: password,
+        password_confirmation: confirmPassword
+      });
+      console.log(res);
 
+      navigate('/pages/login/login3');
+      toast.success(res.data.message);
+      setIsSubmitted(true);
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log(error);
+    }
     // Add your password reset logic here
     // For example, you can send a request to your server to update the password
 
     // Set the submission status to true
-    setIsSubmitted(true);
   };
 
   return (
